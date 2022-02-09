3eb2c3814c1e50f207b886f0993ee90715298fd2
Leo Pöntinen <leo.pontinen@myy.haaga-helia.fi>
2022-02-09 17:05:20 +0200

    First commit

---
+++ src/main/java/com/bookstore/web/BookController.java
@@ -0,0 +1,23 @@
+package com.bookstore.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+    public String form(Model model) {
+        return "index";
+    }
+}
